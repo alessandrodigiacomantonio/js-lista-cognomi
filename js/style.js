11295dc3c37e8383cc80fca19739a32faffc6506
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-08 15:19:19 +0100

    Js: stampa su documento html della lista e posizione utente inserito

--- js/style.js
+++ js/style.js
@@ -1,8 +1,16 @@
 var listaNomi = ['carlo', 'fabio', 'alessio', 'agamennone', 'Fulvio', 'Carlo', 'patroclo', 'luigia', 'fulvio', 'marco', 'giorgia'];
 var nomeUtente = prompt('Come ti chiami?');
 listaNomi.push(nomeUtente);
+var posizioneUtente;
 for (var i = 0; i < listaNomi.length; i++) {
   listaNomi[i] = listaNomi[i].toLowerCase();
 }
 listaNomi.sort();
+posizioneUtente = listaNomi.indexOf(nomeUtente);
+for (i = 0; i < listaNomi.length; i++) {
+document.getElementById('lista').innerHTML += '<li>' + listaNomi[i] + '</li>';
+}
+document.getElementById('lista').style.listStyle = 'none';
+document.getElementById('posizione').innerHTML = 'Sei il numero ' + ++posizioneUtente + ' della lista';
 console.log(listaNomi);
+console.log('Sei il numero ' + ++posizioneUtente + ' della lista');
